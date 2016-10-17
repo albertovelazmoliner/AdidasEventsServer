c8eaf56dbffcbae608e1eae8b98b8b466067c6d5
Alberto Velaz Moliner <alberto.velaz@fieldaware.com>
2016-10-17 10:36:38 +0200

    Add insert record

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express')
 var bodyParser = require('body-parser')
- 
-var app = express()
+var pg = require('pg')
 
+var app = express()
 var port = process.env.PORT || 8080;
+
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,10 +23,22 @@ app.post('/api/runner', function(req, res) {
     var dateString = req.body.date
     var country = req.body.country
     
+    var error;
     console.log("token " + token + "\nemail " + email + "\nfirstName " + firstName + "\nlastName " + lastName);
-
-    res.send({message : "Registration has been completed successfully.\nDo you want to register someone else?"});
-});
+    pg.defaults.ssl = true
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  		if (err) throw err
+  		console.log('Connected to postgres!')
+  		var query = client.query("INSERT INTO runners(email, firstName, lasttname, date, country) values ('" 
+  			+ email + "', '" + firstName + "', '" + lastName + "', '" + dateString + "', '" + country + "')", function (err, result) {
+  			if (err) throw err;
+  			client.end(function (err) {
+      			if (err) throw err;
+      			return res.send({message : "Registration has been completed successfully.\nDo you want to register someone else?", error: err});
+      		})	
+  		})
+  	})  
+})
 
 app.listen(port, function() { 
 	console.log('Adidas Server app is running on http://localhost:' + port)
