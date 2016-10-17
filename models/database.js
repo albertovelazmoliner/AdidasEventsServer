3af5e022599423b8e9ee35cef916ad39af9fff3f
Alberto Velaz Moliner <alberto.velaz@fieldaware.com>
2016-10-17 07:57:49 +0200

    Fix query

--- models/database.js
+++ models/database.js
@@ -6,9 +6,9 @@ pg.connect(process.env.DATABASE_URL, function(err, client) {
   console.log('Connected to postgres!')
 
   client
-    .query('CREATE TABLE runners(email text PRIMARY KEY  not null,
-  						firstname text  not null,
-  						lasttname text  not null, 
-  						date CHAR(30) not null,
-  						country text not null)')
+    .query('CREATE TABLE runners(email varchar(80) PRIMARY KEY not null,
+  						firstname varchar(80)  not null,
+  						lasttname varchar(80)  not null, 
+  						date varchar(80) not null,
+  						country varchar(80) not null)')
 });
